7709509373f12e5753d72a78291202745b4dc536
Xvezda <xvezda@naver.com>
2020-01-14 05:14:10 +0900

    Add wrapper

--- pcap_stat.cpp
+++ pcap_stat.cpp
@@ -1,16 +1,17 @@
 #include "pcap_stat.h"
 
-static char errbuf[PCAP_ERRBUF_SIZE];
 
 
 int main(int argc, char **argv) {
   if (argc != 2) {
-    std::cout << "usage: " << argv[0] << " [pcapfile]" << std::endl;
+    std::cerr << "usage: " << argv[0] << " [pcapfile]" << std::endl;
 
     return EXIT_FAILURE;
   }
   const char* filename = argv[1];
-  pcap_t* handle = pcap_open_offline(filename, errbuf);
+  PcapWrapper wrapper(filename);
+
+  wrapper.next();
 
   return EXIT_SUCCESS;
 }
--- pcap_stat.h
+++ pcap_stat.h
@@ -5,14 +5,43 @@
 
 #include <cstdio>
 #include <cstdlib>
+#include <cstdint>
 
 #include <pcap/pcap.h>
 
-namespace xvzd {
 
+static char errbuf[PCAP_ERRBUF_SIZE];
 
+class PcapWrapper {
+public:
+  PcapWrapper(const char* filename)
+      : header(nullptr), raw_packet(nullptr) {
+    handle = pcap_open_offline(filename, errbuf);
+    // FIXME: std::system_error?
+    if (handle == nullptr) throw std::runtime_error(errbuf);
+  }
 
-}  // namespace xvzd
+  int next() {
+    int result = pcap_next_ex(handle, &header, &raw_packet);
 
+    if (result != 1) return result;
+
+    for (size_t i = 0; i < 20; ++i) {
+      std::printf("%02X ", raw_packet[i]);
+    }
+    return result;
+  }
+
+  virtual ~PcapWrapper() {
+    if (handle != nullptr) {
+      pcap_close(handle);
+    }
+  }
+private:
+  pcap_t* handle;
+
+  struct  pcap_pkthdr* header;
+  const   u_char*      raw_packet;
+};
 
 #endif  // PCAP_STAT_H_
